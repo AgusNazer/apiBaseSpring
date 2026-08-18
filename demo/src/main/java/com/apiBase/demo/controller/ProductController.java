70fce5d08d77dbd8d0e0add89aef0893e92184b9
AgusNazer <agustin.nazer@hotmail.com>
2026-08-17 23:17:38 -0300

    feat: get & get id methods

--- demo/src/main/java/com/apiBase/demo/controller/ProductController.java
+++ demo/src/main/java/com/apiBase/demo/controller/ProductController.java
@@ -1,5 +1,40 @@
 package com.apiBase.demo.controller;
 
+import com.apiBase.demo.model.Product;
+import com.apiBase.demo.repository.IProductRepository;
+import com.apiBase.demo.service.IProductService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
 
+    private final IProductService productService;
+
+    public ProductController(IProductService productService){
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getProducts(){
+        return productService.getProduct();
+    }
+
+    @GetMapping("/{codProduct}")
+    public ResponseEntity<?> getProductById(@PathVariable Long codProduct){
+        Product product = productService.findProductById(codProduct);
+        if(product == null){
+            return (ResponseEntity<?>) ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body("No se encuentra un producto con ese codigo");
+        }
+        return ResponseEntity.ok(product);
+    }
 }
