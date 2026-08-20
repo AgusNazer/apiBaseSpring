5a80d3ddd56f4a4677ccc7a8a2b726c981635824
AgusNazer <agustin.nazer@hotmail.com>
2026-08-20 20:20:11 -0300

    feat: get,getById,create done

--- demo/src/main/java/com/apiBase/demo/service/PersonService.java
+++ demo/src/main/java/com/apiBase/demo/service/PersonService.java
@@ -16,17 +16,21 @@ public class PersonService implements IPersonService{
 
     @Override
     public List<Person> getPerson() {
-        return List.of();
+        return personRepository.findAll();
     }
 
     @Override
     public Person getPersonbyId(Long id) {
-        return null;
+        return personRepository.findById(id).orElse(null);
     }
 
     @Override
     public Person createPerson(Person person) {
-        return null;
+        if(person == null){
+            return null;
+        }
+        //el repository con el moetodo, nos trae el objeto entero ya con id de la db
+        return personRepository.save(person);
     }
 
     @Override
